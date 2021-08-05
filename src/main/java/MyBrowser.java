ad6e2807e8b8ec9abb190ecf70d5c8daa311817c
HouZhiping <657244927@qq.com>
2021-08-05 22:57:08 +0800

    craete

--- src/main/java/MyBrowser.java
+++ src/main/java/MyBrowser.java
@@ -1,24 +1,24 @@
-import com.microsoft.playwright.Browser;
-import com.microsoft.playwright.BrowserType;
-import com.microsoft.playwright.Page;
-import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.*;
 import com.microsoft.playwright.options.Proxy;
 
 public class MyBrowser {
 
 
+    private final static String twitter = "https://twitter.com/wangzhian8848";
 
     public void createBrowser(){
         System.out.println("程序开始");
         try (Playwright playwright = Playwright.create()) {
-            System.out.println("开始创建浏览器");
+            System.out.println("环境准备完成");
             BrowserType.LaunchOptions launchOptions = new BrowserType.LaunchOptions();
             launchOptions.setProxy(new Proxy("http://127.0.0.1:7890"));
             launchOptions.setHeadless(false);
             launchOptions.setTimeout(600000).setSlowMo(1000);
             Browser browser = playwright.chromium().launch(launchOptions);
+            System.out.println("创建浏览器完成");
             System.out.println(browser.isConnected());
-            Page page = browser.newPage();
+            BrowserContext browserContext = browser.newContext();
+            Page page = browserContext.newPage();
             System.out.println(browser.isConnected());
             browser.onDisconnected((a) -> System.out.println("浏览器关闭回调"));
             page.navigate("https://twitter.com/login");
@@ -32,13 +32,106 @@ public class MyBrowser {
             browser.close();
 //            page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("example.png")));
         }
+    }
 
+
+    public void loginTwitter(){
+        try (Playwright playwright = Playwright.create()) {
+            System.out.println("环境准备完成");
+            BrowserType.LaunchOptions launchOptions = new BrowserType.LaunchOptions();
+            launchOptions.setProxy(new Proxy("http://127.0.0.1:7890"));
+            launchOptions.setHeadless(false);
+            launchOptions.setTimeout(600000).setSlowMo(1000);
+            Browser browser = playwright.chromium().launch(launchOptions);
+            System.out.println("创建浏览器完成");
+            System.out.println(browser.isConnected());
+            BrowserContext browserContext = browser.newContext();
+            Page page = browserContext.newPage();
+            System.out.println(browser.isConnected());
+            browser.onDisconnected((a) -> System.out.println("浏览器关闭回调"));
+            page.navigate("https://twitter.com/login");
+            //name="session[username_or_email]"
+            page.fill("[name=\"session[username_or_email]\"]", "@RiverHou");
+            page.fill("[name=\"session[password]\"]", "Hzp199005023");
+            page.click("[data-testid=\"LoginForm_Login_Button\"]");
+            System.out.println("登录");
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(page.title());
+            browser.close();
+//            page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("example.png")));
+        }
+    }
+
+
+    public void test(){
+        try (Playwright playwright = Playwright.create()) {
+            BrowserType webkit = playwright.webkit();
+            Browser browser = webkit.launch(new BrowserType.LaunchOptions().setHeadless(false));
+            BrowserContext context = browser.newContext();
+            context.exposeBinding("pageURL", (source, args) -> source.page().url());
+            context.exposeBinding("clicked", (source, args) -> {
+                ElementHandle element = (ElementHandle) args[0];
+                System.out.println(element.textContent());
+                return null;
+            }, new BrowserContext.ExposeBindingOptions().setHandle(true));
+            Page page = context.newPage();
+            page.setContent("" +
+                    "<script>\n" +
+                    "  document.addEventListener('click', event => window.clicked(event.target));\n" +
+                    "</script>\n" +
+                    "<div>Click me</div>\n" +
+                    "<div>Or click me</div>\n");
+//            page.click("button");
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    public void evalOnSelector(){
+
+        try (Playwright playwright = Playwright.create()) {
+            System.out.println("环境准备完成");
+            BrowserType.LaunchOptions launchOptions = new BrowserType.LaunchOptions();
+            launchOptions.setProxy(new Proxy("http://127.0.0.1:7890"));
+            launchOptions.setHeadless(false);
+            launchOptions.setTimeout(600000).setSlowMo(1000);
+            Browser browser = playwright.chromium().launch(launchOptions);
+            System.out.println("创建浏览器完成");
+            System.out.println(browser.isConnected());
+            BrowserContext browserContext = browser.newContext();
+            Page page = browserContext.newPage();
+            System.out.println(browser.isConnected());
+            browser.onDisconnected((a) -> System.out.println("浏览器关闭回调"));
+            page.navigate(twitter);
+            String sectionText = (String) page
+                    .evalOnSelector("span.css-901oao.css-16my406.r-poiln3.r-bcqeeo.r-qvutc0", "e => e.textContent");
+            //name="session[username_or_email]"
+            System.out.println(sectionText);
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+//            System.out.println(page.title());
+            browser.close();
+//            page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("example.png")));
+        }
 
     }
 
     public static void main(String[] args) {
         MyBrowser myBrowser = new MyBrowser();
-        myBrowser.createBrowser();
+        myBrowser.evalOnSelector();
+//        myBrowser.loginTwitter();
+
     }
 
 
